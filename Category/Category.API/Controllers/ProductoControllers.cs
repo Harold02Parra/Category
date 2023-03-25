018cd5cbccd6360f1f5ff2950452f78bfea80754
Harold Parra <haroldmena310468@correo.itm.edu.co>
2023-03-24 19:41:19 -0500

    commit crud

--- Category/Category.API/Controllers/ProductoControllers.cs
+++ Category/Category.API/Controllers/ProductoControllers.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Category.API.Data;
 using Category.Shared.Entities;
+using System.Diagnostics.Metrics;
 
 namespace Categorys.API.Controllers
 {
@@ -31,5 +32,72 @@ namespace Categorys.API.Controllers
             await _context.SaveChangesAsync();
             return Ok();
         }
+
+        [HttpGet("{id:int}")]  ///api/productos/1
+        public async Task<ActionResult> Get(int id)
+        {
+            var producto = await _context.Productos.FirstOrDefaultAsync(x => x.Id == id);
+            if (producto is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(producto);
+        }
+
+        // Actualización
+
+        [HttpPut]
+        public async Task<ActionResult> Put(Producto producto)
+        {
+
+            try
+            {
+
+                _context.Update(producto);
+                await _context.SaveChangesAsync();
+
+
+                return Ok(producto);
+
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
+                {
+                    return BadRequest("Ya existe un registro con el mismo nombre.");
+                }
+                else
+                {
+                    return BadRequest(dbUpdateException.InnerException.Message);
+                }
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+
+
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var afectedRows = await _context.Productos
+                .Where(x => x.Id == id)
+
+                .ExecuteDeleteAsync();
+
+            if (afectedRows == 0)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+
+
+
     }
 }
